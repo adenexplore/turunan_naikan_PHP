4315e7a44a77dc98d2c6ed52dc191fea78288697
Aden Rifai <adenrifai01@gmail.com>
2023-06-06 10:06:18 +0700

    first name

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nama = array("Siti", "Budi", "Andi", "Dewi", "Eka");
+sort($nama);
+foreach($nama as $value) {
+  echo "$value <br>";
+}
+?>
+
+
+<?php
+$nomor = array(1,100,5,7,6,8,2,3,6,44);
+sort($nomor);
+foreach($nomor as $value) {
+  echo "$value <br>";
+}
+?>
